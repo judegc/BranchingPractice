9378360384b5a42ddafe3909de38d13b59c103a5
Jude Davis <juducation@outlook.com>
2022-08-08 20:06:31 -0400

    Updated Notes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,3 +5,7 @@
 // git branch <branch name> - creates a branch of the same name that is typed
 
 // git checkout <branch name> - switches to te branch of the same name
+
+//cls - Clears PowerShell history
+
+// git branch -D <branch name> - deletes the branch of the same name
