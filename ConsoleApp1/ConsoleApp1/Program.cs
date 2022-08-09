49b921cd552b4d7a65493bb9f09516d0098fd53d
Jude Davis <juducation@outlook.com>
2022-08-08 20:03:29 -0400

    Added Notes to Main

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Branches are copies/different versions of the same project.
+
+//git branch - displays all existing branches
+
+// git branch <branch name> - creates a branch of the same name that is typed
+
+// git checkout <branch name> - switches to te branch of the same name
